3c74d3577260abd6e62d63d9e5acba89b65626b8
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-08-01 00:07:32 +0300

    save changes

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//(((((((((((((((audio /video)))))))))))))))
+// audio controls , muted ,autoplay preload="metadata"
+// video controls autoplay muted 
+// geolocation
+// drag and drop js
+// svg 
+// rect x,y,width,height="12",style="fill:white; stroke:red; stroke-width=2px"
+//line x1,y1,x2,y2 style
+//circle cx,cy,r style
+//ellipse cx,cy,rx,ry style
+//star
+//polygon linearGradient id,x1,y1
